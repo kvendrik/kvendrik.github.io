e7ae26dd3d4a0606d96c2ada9bf8db31fd145d65
Koen Vendrik <k.vendrik@gmail.com>
2016-06-10 22:29:38 +0200

    rm .sass-cache and deploy script

--- _deploy.sh
+++
@@ -1,6 +0,0 @@
-DEPLOY_PATH="/var/www/html/kvendrik.com/www/"
-
-echo "Deploying to $DEPLOY_PATH..."
-
-rsync -av --exclude '.git' --exclude 'node_modules' --exclude '.sass-cache' ./www/ root@37.128.149.176:$DEPLOY_PATH
-
