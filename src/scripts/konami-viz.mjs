fc5b78e6c7bb7d9746d7a8c6abd49a69c1c5c9a0
Koen Vendrik <k.vendrik@gmail.com>
2020-07-28 17:34:19 -0400

    rm private fields as theyre not supported in FF

--- src/scripts/konami-viz.mjs
+++ src/scripts/konami-viz.mjs
@@ -1,10 +1,10 @@
 export default class KonamiCodeViz {
-  #konamiCode = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowright', 'arrowright', 'arrowleft', 'arrowleft', 'b', 'a'];
-  #currentSequenceIndex = 0;
-  #keyElements = null;
-  #handleComplete = null;
-  #options = {}
-  
+  konamiCode = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowright', 'arrowright', 'arrowleft', 'arrowleft', 'b', 'a'];
+  currentSequenceIndex = 0;
+  keyElements = null;
+  handleComplete = null;
+  options = {}
+
   constructor(wrapper, options = {
     Classes: {
       key: 'konami-viz__key',
@@ -12,44 +12,44 @@ export default class KonamiCodeViz {
       keyPressed: 'konami-viz__key--is-pressed',
     },
   }) {
-    this.#keyElements = wrapper.querySelectorAll(`.${options.Classes.key}`);
-    this.#options = options;
+    this.keyElements = wrapper.querySelectorAll(`.${options.Classes.key}`);
+    this.options = options;
   }
-  
+
   bindEvents() {
     document.addEventListener('keyup', this.handleKeyEvent.bind(this));
   }
-  
+
   onComplete(callback) {
-    this.#handleComplete = callback;
+    this.handleComplete = callback;
   }
-  
+
   handleKeyEvent({key}) {
     const lowercaseKey = key.toLowerCase();
-    const nextExpectedKey = this.#konamiCode[this.#currentSequenceIndex];
-    const {Classes} = this.#options;
-    
+    const nextExpectedKey = this.konamiCode[this.currentSequenceIndex];
+    const {Classes} = this.options;
+
     if (lowercaseKey !== nextExpectedKey) {
       this.reset(lowercaseKey);
       return;
     }
-    
-    this.#keyElements[this.#currentSequenceIndex].classList.add(Classes.keyCompleted);
 
-    if (this.#currentSequenceIndex === (this.#konamiCode.length - 1) && this.#handleComplete) {
-      this.#handleComplete();
+    this.keyElements[this.currentSequenceIndex].classList.add(Classes.keyCompleted);
+
+    if (this.currentSequenceIndex === (this.konamiCode.length - 1) && this.handleComplete) {
+      this.handleComplete();
       this.reset();
-      return;  
-    } 
+      return;
+    }
 
-    this.#currentSequenceIndex += 1;
+    this.currentSequenceIndex += 1;
   }
-  
-  reset(highlightKey = null) {
-    const {Classes} = this.#options;
 
-    for (const [index, keyNode] of this.#keyElements.entries()) {
-      const key = this.#konamiCode[index];
+  reset(highlightKey = null) {
+    const {Classes} = this.options;
+
+    for (const [index, keyNode] of this.keyElements.entries()) {
+      const key = this.konamiCode[index];
 
       if (key === highlightKey) {
         keyNode.classList.remove(Classes.keyPressed);
@@ -59,9 +59,9 @@ export default class KonamiCodeViz {
         keyNode.classList.add(Classes.keyPressed);
       }
 
-      keyNode.classList.remove(Classes.keyCompleted); 
+      keyNode.classList.remove(Classes.keyCompleted);
     }
 
-    this.#currentSequenceIndex = 0;
+    this.currentSequenceIndex = 0;
   }
 }
