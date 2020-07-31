3160f7f8c08839a874d9f89f5265be11e4820eab
Koen Vendrik <k.vendrik@gmail.com>
2020-07-31 12:57:32 -0400

    hides konami on browser that don't support modules

--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -1,6 +1,12 @@
 import KonamiCodeViz from './konami-viz.mjs';
 
-const konami = new KonamiCodeViz(document.querySelector('.konami-viz'));
+const konamiNode = document.querySelector('.konami-viz');
+const konami = new KonamiCodeViz();
+
+// make sure the instructions aren't shown for browsers
+// that don't support JS modules as this file won't load
+konamiNode.classList.remove('konami-viz--is-hidden');
+
 konami.bindEvents();
 konami.onComplete(() => {
   document.body.classList.add('body--is-hidden');
