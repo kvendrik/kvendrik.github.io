9d9184e45306207930f54bebe9191683ee42a7d3
Koen Vendrik <k.vendrik@gmail.com>
2024-04-17 12:27:44 +0200

    experiment

--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -1,46 +1,9 @@
 import KonamiCodeViz from './konami-viz.mjs';
 
 const isTouchDevice = 'ontouchstart' in window;
-const isSmallScreen = window.matchMedia('(max-width: 800px)').matches;
 
 if (!isTouchDevice) {
   initializeKonamiCodeUi();
-
-  if (!isSmallScreen) {
-    initializeSectionWaypoints();
-  }
-}
-
-function initializeSectionWaypoints() {
-  const hasIntersectionObserverSupport = "IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype;
-  if (!hasIntersectionObserverSupport) return;
-
-  const sections = document.querySelectorAll("[data-waypoint-section]");
-
-  const observer = new IntersectionObserver((entries) => {
-    let newVisibleEntryTarget = null;
-
-    for (const {isIntersecting, target} of entries) {
-      if (target.getAttribute('data-waypoint-section') === 'active' && isIntersecting) {
-        newVisibleEntryTarget = target;
-      }
-    }
-
-    if (newVisibleEntryTarget !== null) {
-      for (const section of sections) section.setAttribute('data-waypoint-section', 'active');
-      newVisibleEntryTarget.setAttribute('data-waypoint-section', 'visible');
-    }
-  }, {
-    rootMargin: "0px",
-    threshold: 0.65,
-  });
-
-  document.body.classList.add('body--waypoint-sections-active');
-
-  for (const section of sections) {
-    section.setAttribute('data-waypoint-section', 'active');
-    observer.observe(section);
-  }
 }
 
 function initializeKonamiCodeUi() {
